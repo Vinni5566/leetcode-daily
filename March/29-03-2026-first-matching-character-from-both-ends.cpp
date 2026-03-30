98906cc2be1ad0075e6649b107bcc4824b35068d
Vinni Kapoor <vinni3871@gmail.com>
2026-03-30 13:49:33 +0530

    Add new daily challneges

---
+++ March/29-03-2026-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,19 @@
+//Problem Link: https://leetcode.com/problems/first-matching-character-from-both-ends/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+
+        int n = s.length();
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] == s[n-i-1]) return i;
+        }
+
+        return -1;
+        
+    }
+};
